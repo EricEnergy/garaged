e2635f6d82f7855e9d5964f84847db7b2f039da6
Eric Energy <ericnrgnash@gmail.com>
2020-01-08 11:07:15 -0600

    added compression

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var session = require("express-session");
 var passport = require("./config/passport");
 var exphbs = require("express-handlebars");
-
+var compression = require("compression");
 
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
@@ -15,6 +15,17 @@ app.use(express.static("public"));
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(compression())
+function shouldCompress(req, res){
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+ 
+  // fallback to standard filter function
+  return compression.filter(req, res)
+};
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
